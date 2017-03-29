87ce925c6b141dd55f5a9509c324e7b816334f09
kusti8 <kusti8@gmail.com>
2017-03-29 18:12:08 -0400

    Fix read only

--- read-only.sh
+++ read-only.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /boot/check ]; then
   sed -i "s/#kernel=kernel7.img/kernel=kernel7.img/" /boot/config.txt
-  sed "s/#initramfs initrd7.img/initramfs initrd7.img/" /boot/config.txt
-  echo "boot=overlay" >> /boot/cmdline.txt
+  sed -i "s/#initramfs initrd7.img/initramfs initrd7.img/" /boot/config.txt
+  sed -i '$s/$/ boot=overlay/' /boot/cmdline.txt
   touch /boot/check
 fi
